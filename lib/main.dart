e6fa4d64cc11d7022e3f0b4600f5fabb77c52657
Euphoria99 <363pavanbhat@gmail.com>
2023-02-02 23:31:18 +0530

    Scaffold , Center and floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Hello world'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello world'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Body is centered'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('click'),
+      ),
+    ),
   ));
 }
