c4d3c5da6e3fa091b053a3cdd3d773b5c1abfb50
Euphoria99 <363pavanbhat@gmail.com>
2023-02-09 00:31:39 +0530

    world time app init setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,13 @@
 import 'package:flutter/material.dart';
+//
+import 'package:flutter_tour/pages/choose_location.dart';
+import 'package:flutter_tour/pages/home.dart';
+import 'package:flutter_tour/pages/loading.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: MyCard(),
-  ));
-}
-
-class MyCard extends StatefulWidget {
-  const MyCard({super.key});
-
-  @override
-  State<MyCard> createState() => _MyCardState();
-}
-
-class _MyCardState extends State<MyCard> {
-  int vikingLevel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Viking ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('Assets/images/viking.jpg'),
-                radius: 50,
-              ),
-            ),
-            Divider(
-              color: Colors.grey[800],
-              height: 90,
-            ),
-            const Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Askelad',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            const Text(
-              'CURRENT VIKING LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '$vikingLevel',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                const Text(
-                  'askelad@thevikingsofeurope.com',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.amberAccent[200],
-        child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            vikingLevel += 1;
-          });
-        },
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
 }
