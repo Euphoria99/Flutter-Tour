842342b80a445823adc93f9cafa5d1b880273577
Euphoria99 <363pavanbhat@gmail.com>
2023-02-12 19:19:21 +0530

    flutter http package| converting json string into map

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,37 +6,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  // void getData() async {
-  //   //simulate network request for a username
-  //   await Future.delayed(Duration(seconds: 3), () {
-  //     print('username here');
-  //   });
-  //   //simulate network request for a bio
-  //   await Future.delayed(Duration(seconds: 2), () {
-  //     print('bio data here');
-  //   });
-  //   print('hey yo!');
-  // }
-
-  void getData() async {
-    //simulate network request for a username
-    String name = await Future.delayed(Duration(seconds: 3), () {
-      return 'pavan here';
-    });
-    //simulate network request for a bio
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'software developer';
-    });
-    print('$name - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hello there'); //this prints first regardless
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
