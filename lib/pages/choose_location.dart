5895b68f6076cf1613327da9cf74298e4ad130c8
Euphoria99 <363pavanbhat@gmail.com>
2023-02-12 18:07:08 +0530

    Async-Await code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,17 +6,39 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  // void getData() async {
+  //   //simulate network request for a username
+  //   await Future.delayed(Duration(seconds: 3), () {
+  //     print('username here');
+  //   });
+  //   //simulate network request for a bio
+  //   await Future.delayed(Duration(seconds: 2), () {
+  //     print('bio data here');
+  //   });
+  //   print('hey yo!');
+  // }
+
+  void getData() async {
+    //simulate network request for a username
+    String name = await Future.delayed(Duration(seconds: 3), () {
+      return 'pavan here';
+    });
+    //simulate network request for a bio
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'software developer';
+    });
+    print('$name - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('init state works');
+    getData();
+    print('hello there'); //this prints first regardless
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function works');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -24,16 +46,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         backgroundColor: Colors.blue[900],
         centerTitle: true,
         elevation: 0,
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(
-            () {
-              counter += 1;
-            },
-          );
-        },
-        child: Text('counter is $counter'),
       ),
     );
   }
