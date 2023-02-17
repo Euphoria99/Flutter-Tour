855bae28a1d1dbef2e5dae46cf8838f0074c20cc
Euphoria99 <363pavanbhat@gmail.com>
2023-02-17 23:56:48 +0530

    World time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,18 +8,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
+    //make request
     Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+        await get(Uri.parse('http://worldtimeapi.org/api/Asia/Kolkata'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['id']);
+    // print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset1 = data['utc_offset'].substring(1, 3);
+    String offset2 = data['utc_offset'].substring(4, 6);
+    print('date time is' + datetime);
+    // print('offset1 is' + offset1);
+    // print('offset2 is' + offset2);
+
+    //create  DateTime object
+
+    DateTime now = DateTime.parse(datetime);
+    now = now
+        .add(Duration(hours: int.parse(offset1), minutes: int.parse(offset2)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
